1a614b689d98f45a02eacbe08a4da3dfcea7ff1d
PanKunik <patryk.kunicki@pm.me>
2023-09-15 12:38:31 +0200

    Adding test with prepared data for test case

--- tests/MySpot.Tests.Integration/Controllers/UsersControllerTests.cs
+++ tests/MySpot.Tests.Integration/Controllers/UsersControllerTests.cs
@@ -3,6 +3,7 @@ using System.Net.Http.Json;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using MySpot.Application.Commands;
+using MySpot.Application.DTO;
 using MySpot.Core.Entities;
 using MySpot.Core.ValueObjects;
 using MySpot.Infrastructure.Security;
@@ -39,6 +40,40 @@ public class UserControllerTests : ControllerTests, IDisposable
 
         // Assert
         response.StatusCode.ShouldBe(HttpStatusCode.Created);
+    }
+
+    [Fact]
+    public async Task SignIn_WhenInvokedWithValidCredentials_ShouldReturn200OkStatusCodeAndJwt()
+    {
+        // Arrange
+        var passwordManager = new PasswordManager(
+            new PasswordHasher<User>()
+        );
+        var password = "secretpassword";
+        var email = "test-user@myspot.io";
+        var user = new User(
+            new UserId(Guid.NewGuid()),
+            new Email(email),
+            new Username("test-user"),
+            passwordManager.Secure(password),
+            new FullName("Test User"),
+            Role.User(),
+            DateTime.Now
+        );
+
+        await _database.Context.Database.MigrateAsync();
+        await _database.Context.Users.AddAsync(user);
+        await _database.Context.SaveChangesAsync();
+
+        var signIn = new SignIn(email, password);
+
+        // Act
+        var response = await Client.PostAsJsonAsync("users/sign-in", signIn);
+
+        // Assert
+        var jwt = await response.Content.ReadFromJsonAsync<JwtDto>();
+        jwt.ShouldNotBeNull();
+        jwt.AccessToken.ShouldNotBeNullOrWhiteSpace();
     }
 
     public void Dispose()
